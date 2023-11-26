02bb3bc9ad9f8a38b58b3eb40fe0ba75c5048744
Rahil Bin Mushfiq <rahilbinmushfiq@gmail.com>
2023-11-26 13:54:25 +0600

    feat: add new fonts and configure Tailwind
    The changes in include:
    - Update the Tailwind config file to extend the font family with new fonts
    - Import the newly added "Passion One" and "Nunito" fonts in the main layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "passion-one": ["var(--font-passion-one)", ...fontFamily.sans],
+        nunito: ["var(--font-nunito)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
